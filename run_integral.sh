52f57b1a2292169dbb81270146fac18c776aa6bb
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-09-02 21:47:48 -0500

    Add rest of test cases

--- run_integral.sh
+++ run_integral.sh
@@ -2,8 +2,15 @@
 file="integral"
 
 mpicc src/${file}.c -o out/${file}
-mpirun -np 64 -hostfile ~/hostfile out/${file} < tests/test_1_input.txt > tests/test_1_output.txt
+
+mpirun -np 4 -hostfile ~/hostfile out/${file} < tests/test_1_input.txt > tests/test_1_output.txt
 diff tests/test_1_output.txt tests/test_1_expected_output.txt
+
+mpirun -np 4 -hostfile ~/hostfile out/${file} < tests/test_2_input.txt > tests/test_2_output.txt
+diff tests/test_2_output.txt tests/test_2_expected_output.txt
+
+mpirun -np 4 -hostfile ~/hostfile out/${file} < tests/test_3_input.txt > tests/test_3_output.txt
+diff tests/test_3_output.txt tests/test_3_expected_output.txt
 
 #mpirun -np 4 -hostfile ~/hostfile out/${file}
 #mpirun -np 2 out/${file}
